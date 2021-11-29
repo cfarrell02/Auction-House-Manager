359915d2b9aa83a095e2bc3dad02738cea1ab503
Cian Farrell <cianster24@gmail.com>
2021-11-29 09:18:21 +0000

    Initial Files

---
+++ src/main/java/Main/MainController.java
@@ -0,0 +1,16 @@
+package Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
+public class MainController {
+
+
+
+
+}
